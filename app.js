02b5ee93d182929416cb00ffc2e1a1c736d028bd
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-10-31 15:08:42 +0330

    add router to app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const userRouter = require("./server/routes/userRouter")
+
 const app = express();
 
 if (process.env.NODE_ENV == "development") app.use(morgan("dev"));
@@ -13,5 +15,7 @@ app.use((req, res, next) => {
   console.log("App started!");
   next();
 });
+
+app.use("/api/users", userRouter);
 
 module.exports = app;
